18ecdf6277b4fda06dcef90b457df9fe3257e634
Baglyas <117549777+Baglyas@users.noreply.github.com>
2023-06-09 02:36:18 +0200

    Error
    onSubmit the motion.div flickers

--- src/app/Components/Inputs.tsx
+++ src/app/Components/Inputs.tsx
@@ -26,42 +26,96 @@ import { AnimatePresence, motion } from "framer-motion";
 const Inputs = () => {
   const user: User = useSelector((state: { user: User }) => state.user);
   const dispatch = useDispatch();
+  const [selectedId, setSelectedId] = useState<any>(null);
+
 
   const Name = () => {
     return (
-      <>
-        <motion.div layoutId="Name">
-          <motion.input
-            type="text"
-            value={user.name}
-            onChange={(e) => dispatch(getName(e.target.value))}
-          />
-          Name is: {user.name}
-          <motion.input
-            type="number"
-            onChange={(e) => dispatch(getAge(e.target.value))}
-          />
-          Age is: {user.age.toString()}
-          <motion.button onClick={() => setSelectedId(null)}>X</motion.button>
-        </motion.div>
-      </>
+      <motion.div layoutId="Name">
+        <motion.input
+          type="text"
+          value={user.name}
+          onChange={(e) => dispatch(getName(e.target.value))}
+        />
+        Name is: {user.name}
+        <motion.input
+          type="number"
+          onChange={(e) => dispatch(getAge(e.target.value))}
+        />
+        Age is: {user.age.toString()}
+        <motion.button onClick={() => setSelectedId(null)}>X</motion.button>
+      </motion.div>
     );
   };
 
   // Experiences
-  interface Experiences {
-    jobTitle?: String;
-    date?: Number;
-    description?: String;
-  }
-  const [experiences, setExperiences] = useState<Experiences>({
-    jobTitle: "",
-    date: undefined,
-    description: "",
-  });
-
+  
   const Experiences = () => {
-    return <>exp</>;
+    interface Experiences {
+      jobTitle?: String;
+      date?: Number;
+      description?: String;
+    }
+    const [experiences, setExperiences] = useState<Experiences>({
+      jobTitle: "",
+      date: undefined,
+      description: "",
+    });
+    return (
+      <motion.div layoutId="Experiences">
+        {/* Experiences */}
+
+        <FormGroup>
+          <Typography>Your experiences:</Typography>
+          <Input
+            placeholder="Date"
+            type="number"
+            value={experiences.date}
+            onChange={(e) => {
+              setExperiences((current) => ({
+                ...current,
+                date: Number(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="Job Title"
+            type="text"
+            value={experiences.jobTitle}
+            onChange={(e) => {
+              setExperiences((current) => ({
+                ...current,
+                jobTitle: String(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="description"
+            type="text"
+            value={experiences.description}
+            onChange={(e) => {
+              setExperiences((current) => ({
+                ...current,
+                description: e.target.value,
+              }));
+            }}
+          />
+          <Button
+            variant="contained"
+            onClick={(e) => {
+              dispatch(getExperiences(experiences));
+              //default values
+              setExperiences({
+                jobTitle: "",
+                date: undefined,
+                description: "",
+              });
+            }}>
+            Submit!
+          </Button>
+        </FormGroup>
+      </motion.div>
+    );
   };
 
   // Education
@@ -78,7 +132,71 @@ const Inputs = () => {
     major: "",
   });
   const Education = () => {
-    return <>edu</>;
+    return (
+      <motion.div layoutId="Education">
+        {/* Education */}
+
+        <FormGroup>
+          <Input
+            type="text"
+            placeholder="School name:"
+            value={educations.schoolName}
+            onChange={(e) => {
+              setEducations((current) => ({
+                ...current,
+                schoolName: String(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="Starting year"
+            value={educations.start}
+            type="number"
+            onChange={(e) => {
+              setEducations((current) => ({
+                ...current,
+                start: Number(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="Graduation year"
+            type="number"
+            value={educations.finish}
+            onChange={(e) => {
+              setEducations((current) => ({
+                ...current,
+                finish: Number(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="Major (optional)"
+            type="text"
+            value={educations.major}
+            onChange={(e) => {
+              setEducations((current) => ({
+                ...current,
+                major: String(e.target.value),
+              }));
+            }}
+          />
+          <Button
+            onClick={(e) => {
+              dispatch(getEducation(educations));
+              //Reset educations
+              setEducations({
+                start: undefined,
+                finish: undefined,
+                schoolName: "",
+                major: "",
+              });
+            }}>
+            Submit!
+          </Button>
+        </FormGroup>
+      </motion.div>
+    );
   };
   //Contacts
 
@@ -95,12 +213,82 @@ const Inputs = () => {
     website: "",
   });
   const Contacts = () => {
-    return <>contact</>;
+    return (
+      <motion.div layoutId="Contacts">
+        <FormGroup>
+          <Input
+            placeholder="Email address"
+            value={contacts.email}
+            type="text"
+            onChange={(e) => {
+              setContacts((current) => ({
+                ...current,
+                email: e.target.value,
+              }));
+            }}
+          />
+          <Input
+            placeholder="Address"
+            value={contacts.address}
+            type="text"
+            onChange={(e) => {
+              setContacts((current) => ({
+                ...current,
+                address: e.target.value,
+              }));
+            }}
+          />
+          <Input
+            placeholder="Phone number"
+            value={contacts.phone}
+            type="num"
+            onChange={(e) => {
+              setContacts((current) => ({
+                ...current,
+                phone: Number(e.target.value),
+              }));
+            }}
+          />
+          <Input
+            placeholder="Website"
+            value={contacts.website}
+            type="text"
+            onChange={(e) => {
+              setContacts((current) => ({
+                ...current,
+                website: String(e.target.value),
+              }));
+            }}
+          />
+          <Button
+            onClick={() => {
+              dispatch(getContacts(contacts));
+            }}>
+            Submit!
+          </Button>
+        </FormGroup>
+      </motion.div>
+    );
   };
   //Skills
   const [skills, setSkills] = useState("");
   const Skills = () => {
-    return <>skill</>;
+    return (
+      <motion.div layoutId="Skills">
+        <Input
+          placeholder="Skills"
+          value={skills}
+          onChange={(e) => setSkills(e.target.value)}
+        />
+        <Button
+          onClick={() => {
+            dispatch(getSkills(skills));
+            setSkills("");
+          }}>
+          Submit!
+        </Button>
+      </motion.div>
+    );
   };
   //Hobbies
   interface Hobbies {
@@ -112,7 +300,52 @@ const Inputs = () => {
     hobby: "",
   });
   const Hobbies = () => {
-    return <></>;
+    return (
+      <motion.div layoutId="Hobbies">
+        <Input
+          placeholder="Hobbies"
+          value={hobbies.hobby}
+          onChange={(e) => {
+            setHobbies({
+              ...hobbies,
+              hobby: e.target.value,
+            });
+          }}
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={hobbies.icon}
+            label="Age"
+            onChange={(e) =>
+              setHobbies({
+                ...hobbies,
+                icon: e.target.value,
+              })
+            }>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/**Make here some custom made list of icons */}
+        <Button
+          onClick={() => {
+            hobbies.hobby !== ""
+              ? dispatch(getHobbies(hobbies))
+              : alert("Hobbies input is empty!");
+            setHobbies({
+              icon: undefined,
+              hobby: "",
+            });
+          }}>
+          Submit!
+        </Button>
+      </motion.div>
+    );
   };
   const categories = [
     "Name",
@@ -124,7 +357,6 @@ const Inputs = () => {
   ];
   const components = [Name, Experiences, Education, Contacts, Skills, Hobbies];
 
-  const [selectedId, setSelectedId] = useState<any>(null);
   return (
     <div>
       <div style={{}}>
@@ -152,223 +384,6 @@ const Inputs = () => {
         }
         return null;
       })}
-
-      {/* Experiences */}
-
-      <FormGroup>
-        <Typography>Your experiences:</Typography>
-        <Input
-          placeholder="Date"
-          type="number"
-          value={experiences.date}
-          onChange={(e) => {
-            setExperiences((current) => ({
-              ...current,
-              date: Number(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="Job Title"
-          type="text"
-          value={experiences.jobTitle}
-          onChange={(e) => {
-            setExperiences((current) => ({
-              ...current,
-              jobTitle: String(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="description"
-          type="text"
-          value={experiences.description}
-          onChange={(e) => {
-            setExperiences((current) => ({
-              ...current,
-              description: e.target.value,
-            }));
-          }}
-        />
-        <Button
-          variant="contained"
-          onClick={(e) => {
-            dispatch(getExperiences(experiences));
-            //default values
-            setExperiences({ jobTitle: "", date: undefined, description: "" });
-          }}>
-          Submit!
-        </Button>
-      </FormGroup>
-
-      {/* Education */}
-
-      <FormGroup>
-        <Input
-          type="text"
-          placeholder="School name:"
-          value={educations.schoolName}
-          onChange={(e) => {
-            setEducations((current) => ({
-              ...current,
-              schoolName: String(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="Starting year"
-          value={educations.start}
-          type="number"
-          onChange={(e) => {
-            setEducations((current) => ({
-              ...current,
-              start: Number(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="Graduation year"
-          type="number"
-          value={educations.finish}
-          onChange={(e) => {
-            setEducations((current) => ({
-              ...current,
-              finish: Number(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="Major (optional)"
-          type="text"
-          value={educations.major}
-          onChange={(e) => {
-            setEducations((current) => ({
-              ...current,
-              major: String(e.target.value),
-            }));
-          }}
-        />
-        <Button
-          onClick={(e) => {
-            dispatch(getEducation(educations));
-            //Reset educations
-            setEducations({
-              start: undefined,
-              finish: undefined,
-              schoolName: "",
-              major: "",
-            });
-          }}>
-          Submit!
-        </Button>
-      </FormGroup>
-      <FormGroup>
-        <Input
-          placeholder="Email address"
-          value={contacts.email}
-          type="text"
-          onChange={(e) => {
-            setContacts((current) => ({
-              ...current,
-              email: e.target.value,
-            }));
-          }}
-        />
-        <Input
-          placeholder="Address"
-          value={contacts.address}
-          type="text"
-          onChange={(e) => {
-            setContacts((current) => ({
-              ...current,
-              address: e.target.value,
-            }));
-          }}
-        />
-        <Input
-          placeholder="Phone number"
-          value={contacts.phone}
-          type="num"
-          onChange={(e) => {
-            setContacts((current) => ({
-              ...current,
-              phone: Number(e.target.value),
-            }));
-          }}
-        />
-        <Input
-          placeholder="Website"
-          value={contacts.website}
-          type="text"
-          onChange={(e) => {
-            setContacts((current) => ({
-              ...current,
-              website: String(e.target.value),
-            }));
-          }}
-        />
-        <Button
-          onClick={() => {
-            dispatch(getContacts(contacts));
-          }}>
-          Submit!
-        </Button>
-      </FormGroup>
-      <Input
-        placeholder="Skills"
-        value={skills}
-        onChange={(e) => setSkills(e.target.value)}
-      />
-      <Button
-        onClick={() => {
-          dispatch(getSkills(skills));
-          setSkills("");
-        }}>
-        Submit!
-      </Button>
-
-      <Input
-        placeholder="Hobbies"
-        value={hobbies.hobby}
-        onChange={(e) => {
-          setHobbies({
-            ...hobbies,
-            hobby: e.target.value,
-          });
-        }}
-      />
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={hobbies.icon}
-          label="Age"
-          onChange={(e) =>
-            setHobbies({
-              ...hobbies,
-              icon: e.target.value,
-            })
-          }>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-
-      {/**Make here some custom made list of icons */}
-      <Button
-        onClick={() => {
-          hobbies.hobby !== ""
-            ? dispatch(getHobbies(hobbies))
-            : alert("Hobbies input is empty!");
-          setHobbies({
-            icon: undefined,
-            hobby: "",
-          });
-        }}>
-        Submit!
-      </Button>
     </div>
   );
 };
